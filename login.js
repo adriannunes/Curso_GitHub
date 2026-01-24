6e3eabf8380dc75242badf636961370dfdec7442
Adrian <seuemail@email.com>
2026-01-24 11:26:49 -0300

    Nova home finalizada

--- login.js
+++ login.js
@@ -1,3 +1,12 @@
 // Trabalhando no login
 
 // Finalizando sistema de login
+
+function login(nome, email){
+    return 'Bem vindo'
+}
+
+//Seja bem vindo
+
+//Sistema de login completo
+//Retornamos o nome do usuario quando logar 
